fe57d5952214738df16cf079bd5663a46364efbe
aghalkd <136778751+aghalkd@users.noreply.github.com>
2023-06-20 10:16:16 +0700

    fix dependency import

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import sys
-sys.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import gradio as gr
 from tugas_akhir_module import predict_image, train_update, evaluate_model, change_dropdown, list_model

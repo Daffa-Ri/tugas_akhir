c444d2d018126701f5e5b789e2c344201a0c6334
Daffa <136778751+Daffa-Ri@users.noreply.github.com>
2023-06-24 01:10:51 -0700

    fix

--- tugas_akhir_module/get_model_hash.py
+++ tugas_akhir_module/get_model_hash.py
@@ -5,7 +5,7 @@ from .read_yaml import config_model_location
 def get_model_hash(name: str):
     filename = config_model_location()+name
     with open(filename,"rb") as f:
-    bytes = f.read() # read entire file as bytes
-    readable_hash = hashlib.sha256(bytes).hexdigest();
+        bytes = f.read() # read entire file as bytes
+        readable_hash = hashlib.sha256(bytes).hexdigest();
 
     return readable_hash
